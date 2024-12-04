1a2a73b1890e27aff44e2c5c7add0c3581e78334
Miguelmdfg <miguelfuenmayor2002@gmail.com>
2024-12-04 11:51:58 -0400

    .

--- Curso_C++/Curso C++ UCV/11.determinar_mayor.cpp
+++ Curso_C++/Curso C++ UCV/11.determinar_mayor.cpp
@@ -14,12 +14,38 @@ int main(){
     if (n1 == n2){
         cout << "Ambos numeros son iguales"; 
     }else if(n1 > n2){
-        cout << "El mayor es: " << n1
+        cout << "El mayor es: " << n1;
     }else{
-        cout << "El mayor es: " <<n2
+        cout << "El mayor es: " <<  n2;
     }
     return 0;
 }
 
 
 // Escribe un programa que lea 3 numeros y determine cual es mayor
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, num3;
+
+    // Leer tres números
+    cout << "Introduce el primer número: ";
+    cin >> num1;
+    cout << "Introduce el segundo número: ";
+    cin >> num2;
+    cout << "Introduce el tercer número: ";
+    cin >> num3;
+
+    // Determinar el mayor número
+    if (num1 >= num2 && num1 >= num3) {
+        cout << "El mayor número es: " << num1 << endl;
+    } else if (num2 >= num1 && num2 >= num3) {
+        cout << "El mayor número es: " << num2 << endl;
+    } else {
+        cout << "El mayor número es: " << num3 << endl;
+    }
+
+    return 0;
+}
